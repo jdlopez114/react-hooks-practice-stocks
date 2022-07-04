b4ca2a682b6a6b739a759b104d7ee85f940ec8de
Josiel <jdlopez114@gmail.com>
2022-07-04 00:08:48 -0400

    stocks render, add to portfolio works

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+const [stocks, setStocks] = useState([]);
+const [myStocks, setMyStocks] = useState([]);
+
+function handleClick(stock){
+  if(!myStocks.includes(stock)){
+    const updatedStocks = [...myStocks, stock]
+    setMyStocks(updatedStocks)
+  }
+}
+
+useEffect(() => {
+  fetch(`http://localhost:3001/stocks`)
+  .then(r => r.json())
+  .then(data => setStocks(data))
+}, [])
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} handleClick={handleClick}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer myStocks={myStocks}/>
         </div>
       </div>
     </div>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({ myStocks }) {
+
+  const stockList = myStocks.map(stock => {
+    return <Stock 
+            key={stock.id} 
+            stock={stock}
+            // handleClick={handleClick}
+            />
+  })
+
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {stockList}
     </div>
   );
 }
